f70ecb311de59af909147b855e44a35d57f11a3f
samake-2T2 <jithub@naver.com>
2022-03-24 17:55:27 +0900

    SpringBoot day01

---
+++ Basic/src/main/java/com/simple/basic/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.simple.basic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "hello world";
+	}
+	
+}
