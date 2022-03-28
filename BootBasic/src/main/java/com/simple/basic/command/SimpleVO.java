564e896835d862aa719600d3ca3a19f5f34babcc
samake-2T2 <jithub@naver.com>
2022-03-28 21:52:01 +0900

    SpringBoot day03

---
+++ BootBasic/src/main/java/com/simple/basic/command/SimpleVO.java
@@ -0,0 +1,21 @@
+package com.simple.basic.command;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SimpleVO {
+	// spring boot에서는 vo를 표기할때, wrapper형으로 선언
+	private Integer a; // 정수형
+	private String first; // 문자형
+	private String last;
+	private LocalDateTime regdate; // 날짜형
+	
+}
